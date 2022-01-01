fae45e65610a6bc4d698eaa18c74f1bbea7e2a6a
doublevgp <wgp330522@sina.com>
2022-01-01 15:04:24 +0800

    testCFrace1608

---
+++ contest/1608/d/d.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define uint unsigned int
+#define ull unsigned long long
+template <typename T>
+#define pair2 pair<T, T>
+#define pii pair<int, int>
+#define pli pair<ll, int>
+#define pll pair<ll, ll>
+
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define fi first
+#define se second
+
+const int N = 1e5 + 5;
+
+void solve() {
+
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
